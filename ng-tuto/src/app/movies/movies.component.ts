5e9045e7ddbfc5be5b7a72253d23d260a87e4be1
Joffrey Chambrin <joffrey.chambrin@gmail.com>
2018-12-23 18:25:53 +0100

    Corrections step10

--- ng-tuto/src/app/movies/movies.component.ts
+++ ng-tuto/src/app/movies/movies.component.ts
@@ -1,6 +1,8 @@
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { MoviesService } from './services/movies.service';
 import { Movie } from './../models/movie.model';
 import { Component, OnInit } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movies',
@@ -9,13 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
+  filterGroup: FormGroup;
   movies: Movie[];
+  moviesFiltered: Movie[];
 
-  constructor(private moviesService: MoviesService) { }
+  constructor(private moviesService: MoviesService, fb: FormBuilder) {
+    this.filterGroup = fb.group({
+      filter: new FormControl()
+    });
+  }
 
   ngOnInit() {
     this.moviesService.getMovies().subscribe((movies: Movie[]) => {
       this.movies = movies;
+      this.moviesFiltered = movies;
+    });
+
+    this.filterGroup.get('filter').valueChanges.pipe(
+      debounceTime(200)
+    ).subscribe((str: string) => {
+      this.moviesFiltered = this.movies.filter((m: Movie) => m.name.includes(str));
+      console.log(this.moviesFiltered);
     });
   }
 
